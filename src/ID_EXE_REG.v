e3f80059c2d4c7f164c55b5bea6a399fa55661f2
yihaiyang <haiyangyi873@gmail.com>
2026-08-20 03:26:39 -0400

    1

--- src/ID_EXE_REG.v
+++ src/ID_EXE_REG.v
@@ -9,35 +9,23 @@ module ID_EXE_REG
     //-------------------------
     // 数据通路
     //-------------------------
-
     input wire [31:0] id_pc_i,
-
     input wire [31:0] id_rs1_data_i,
     input wire [31:0] id_rs2_data_i,
-
     input wire [31:0] id_imm_i,
-
     input wire [4:0] id_rd_i,
 
-
-    //-------------------------
+    //------------------------
     // decoder控制信号
     //-------------------------
-
     input wire id_load_i,
-
     input wire id_store_i,
-
     input wire id_branch_i,
-
     input wire id_jump_i,
-
 
     // 写回控制
     input wire id_rd_valid_i,
-
     input wire [1:0] id_wb_sel_i,
-
 
     // 跳转控制
     input wire id_jump_base_rs1_i,
@@ -45,16 +33,11 @@ module ID_EXE_REG
 
     // memory控制
     input wire [1:0] id_mem_size_i,
-
     input wire id_mem_unsigned_i,
 
-
     // ALU输入选择
-
     input wire id_alu_rs1_sel_i,
-
     input wire id_alu_rs2_sel_i,
-
 
     //-------------------------
     // ALU控制(one-hot)
@@ -62,44 +45,30 @@ module ID_EXE_REG
 
     input wire id_alu_add_en_i,
     input wire id_alu_sub_en_i,
-
     input wire id_alu_sll_en_i,
-
     input wire id_alu_slt_en_i,
-
     input wire id_alu_sltu_en_i,
-
     input wire id_alu_xor_en_i,
-
     input wire id_alu_srl_en_i,
-
     input wire id_alu_sra_en_i,
-
     input wire id_alu_or_en_i,
-
     input wire id_alu_and_en_i,
-
     input wire id_alu_pass_b_en_i,
 
-    input reg [7:0]  Out_GPHT_Index_i,
-    input reg [7:0]  Out_LPHT_Index_i,
-    input reg        Out_GPHT_Pred_i,
-    input reg        Out_LPHT_Pred_i,   
-    input reg [31:0] pre_pc_i,
-    input reg        Pred_Taken_i,
+    input wire [7:0]  Out_GPHT_Index_i,
+    input wire [7:0]  Out_LPHT_Index_i,
+    input wire        Out_GPHT_Pred_i,
+    input wire        Out_LPHT_Pred_i,   
+    input wire [31:0] pre_pc_i,
+    input wire        Pred_Taken_i,
  //-------------------------
     // EX阶段
     //-------------------------
 
     output reg [31:0] ex_pc_o,
-
     output reg [31:0] ex_rs1_data_o,
-
     output reg [31:0] ex_rs2_data_o,
-
     output reg [31:0] ex_imm_o,
-
-
     output reg [4:0] ex_rd_o,
 
 
@@ -108,57 +77,32 @@ module ID_EXE_REG
     //-------------------------
 
     output reg ex_load_o,
-
     output reg ex_store_o,
-
     output reg ex_branch_o,
-
     output reg ex_jump_o,
-
-
     output reg ex_rd_valid_o,
-
-
     output reg [1:0] ex_wb_sel_o,
-
-
     output reg ex_jump_base_rs1_o,
     output reg [2:0]  ex_branch_type_o,
-
     output reg [1:0] ex_mem_size_o,
-
     output reg ex_mem_unsigned_o,
-
-
     output reg ex_alu_rs1_sel_o,
-
     output reg ex_alu_rs2_sel_o,
-
 
     //-------------------------
     // ALU控制
     //-------------------------
 
     output reg ex_alu_add_en_o,
-
     output reg ex_alu_sub_en_o,
-
     output reg ex_alu_sll_en_o,
-
     output reg ex_alu_slt_en_o,
-
     output reg ex_alu_sltu_en_o,
-
     output reg ex_alu_xor_en_o,
-
     output reg ex_alu_srl_en_o,
-
     output reg ex_alu_sra_en_o,
-
     output reg ex_alu_or_en_o,
-
     output reg ex_alu_and_en_o,
-
     output reg ex_alu_pass_b_en_o
     output reg [7:0]  Out_GPHT_Index_o,
     output reg [7:0]  Out_LPHT_Index_o,
@@ -175,43 +119,28 @@ module ID_EXE_REG
 
     // 异步复位 全部清零
     if (rst) begin
-
         ex_pc_o <= 32'h0;
-
         ex_rs1_data_o <= 32'h0;
         ex_rs2_data_o <= 32'h0;
-
         ex_imm_o <= 32'h0;
-
         ex_rd_o <= 5'h0;
-
-
         // 控制信号
         ex_load_o <= 1'b0;
         ex_store_o <= 1'b0;
         ex_branch_o <= 1'b0;
         ex_jump_o <= 1'b0;
-
-
         // 写回控制
         ex_rd_valid_o <= 1'b0;
         ex_wb_sel_o <= 2'b00;
-
-
         // 跳转控制
         ex_jump_base_rs1_o <= 1'b0;
         ex_branch_type_o <= 3'b0;
-
         // memory控制
         ex_mem_size_o <= 2'b00;
         ex_mem_unsigned_o <= 1'b0;
-
-
         // ALU输入选择
         ex_alu_rs1_sel_o <= 1'b0;
         ex_alu_rs2_sel_o <= 1'b0;
-
-
         // ALU控制
         ex_alu_add_en_o <= 1'b0;
         ex_alu_sub_en_o <= 1'b0;
@@ -224,7 +153,6 @@ module ID_EXE_REG
         ex_alu_or_en_o <= 1'b0;
         ex_alu_and_en_o <= 1'b0;
         ex_alu_pass_b_en_o <= 1'b0;
-
         //
         Out_GPHT_Index_o <= 8'b0;
         Out_LPHT_Index_o <= 8'b0;
@@ -240,38 +168,23 @@ module ID_EXE_REG
 
         // 数据清零
         ex_pc_o <= 32'h0;
-
         ex_rs1_data_o <= 32'h0;
         ex_rs2_data_o <= 32'h0;
-
         ex_imm_o <= 32'h0;
-
         ex_rd_o <= 5'h0;
-
-
         // 关闭所有控制
         ex_load_o <= 1'b0;
         ex_store_o <= 1'b0;
         ex_branch_o <= 1'b0;
         ex_jump_o <= 1'b0;
-
-
         ex_rd_valid_o <= 1'b0;
         ex_wb_sel_o <= 2'b00;
-
-
         ex_jump_base_rs1_o <= 1'b0;
         ex_branch_type_o <= 3'b0;
-
-
         ex_mem_size_o <= 2'b00;
         ex_mem_unsigned_o <= 1'b0;
-
-
         ex_alu_rs1_sel_o <= 1'b0;
         ex_alu_rs2_sel_o <= 1'b0;
-
-
         ex_alu_add_en_o <= 1'b0;
         ex_alu_sub_en_o <= 1'b0;
         ex_alu_sll_en_o <= 1'b0;
@@ -283,7 +196,6 @@ module ID_EXE_REG
         ex_alu_or_en_o <= 1'b0;
         ex_alu_and_en_o <= 1'b0;
         ex_alu_pass_b_en_o <= 1'b0;
-
         Out_GPHT_Index_o <= 8'b0;
         Out_LPHT_Index_o <= 8'b0;
         Out_GPHT_Pred_o <= 1'b0;
@@ -295,79 +207,50 @@ module ID_EXE_REG
 
     // 正常流水传递
     else if (!stall_i) begin
-
         // 数据
         ex_pc_o <= id_pc_i;
-
         ex_rs1_data_o <= id_rs1_data_i;
         ex_rs2_data_o <= id_rs2_data_i;
-
         ex_imm_o <= id_imm_i;
-
         ex_rd_o <= id_rd_i;
-
-
         // 控制信号
         ex_load_o <= id_load_i;
         ex_store_o <= id_store_i;
         ex_branch_o <= id_branch_i;
         ex_jump_o <= id_jump_i;
-
-
         // 写回
         ex_rd_valid_o <= id_rd_valid_i;
         ex_wb_sel_o <= id_wb_sel_i;
-
-
         // 跳转
         ex_jump_base_rs1_o <= id_jump_base_rs1_i;
         ex_branch_type_o <= id_branch_type_i;
-
-
         // memory
         ex_mem_size_o <= id_mem_size_i;
         ex_mem_unsigned_o <= id_mem_unsigned_i;
-
-
         // ALU输入选择
         ex_alu_rs1_sel_o <= id_alu_rs1_sel_i;
         ex_alu_rs2_sel_o <= id_alu_rs2_sel_i;
-
-
         // ALU one-hot控制
         ex_alu_add_en_o <= id_alu_add_en_i;
         ex_alu_sub_en_o <= id_alu_sub_en_i;
-
         ex_alu_sll_en_o <= id_alu_sll_en_i;
-
         ex_alu_slt_en_o <= id_alu_slt_en_i;
-
         ex_alu_sltu_en_o <= id_alu_sltu_en_i;
-
         ex_alu_xor_en_o <= id_alu_xor_en_i;
-
         ex_alu_srl_en_o <= id_alu_srl_en_i;
-
         ex_alu_sra_en_o <= id_alu_sra_en_i;
-
         ex_alu_or_en_o <= id_alu_or_en_i;
-
         ex_alu_and_en_o <= id_alu_and_en_i;
-
         ex_alu_pass_b_en_o <= id_alu_pass_b_en_i;
-
         Out_GPHT_Index_o <= Out_GPHT_Index_i;
         Out_LPHT_Index_o <= Out_LPHT_Index_i;
         Out_GPHT_Pred_o <= Out_GPHT_Pred_i;
         Out_LPHT_Pred_o <= Out_LPHT_Pred_i;   
         pre_pc_o <= pre_pc_i;
         Pred_Taken_o <= Pred_Taken_i;
-
     end
-
     // stall_i == 1:
     // 不执行任何操作，保持当前EX寄存器内容
-
 end
 
     
